52c101dd1865f6dc50b55cb70b4fa2cb7c80fb38
JHyuk2 <whdgurdl77@gmail.com>
2020-03-18 22:39:36 +0900

    Add daily-0316

--- daily/0316/index.js
+++ daily/0316/index.js
@@ -5,11 +5,19 @@ form.addEventListener('submit', function(event) {
   // 각 변수에 담기 위한 코드 입니다. 수정을 금합니다.
   event.preventDefault();
   const formData = new FormData(event.currentTarget);
-  console.log(formData) // 출력되는 값이 없습니다.
   const userName = formData.get('username');
   const password = formData.get('password');
   const passwordConfirmation = formData.get('password_confirmation');
   // 개발자 도구를 통해 해당 값들을 모두 확인하세요.
   console.log(userName, password, passwordConfirmation);
   // 위의 값들을 활용하여, 회원가입 로직을 작성하시오.
+  if(users.includes(userName)) {
+    alert('존재하는 회원입니다.');
+    return false
+  }
+  if(password != passwordConfirmation) {
+    alert('비밀번호가 일치하지 않습니다.')
+    return false
+  }
+  alert((userName)+'님, 회원가입을 축하합니다.')
 })
