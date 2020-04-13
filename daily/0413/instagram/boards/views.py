26634e780ae64ae42aebaa4a84dec6a1ea959348
JHyuk2 <whdgurdl77@gmail.com>
2020-04-13 19:47:47 +0900

    Add urls, views, forms

--- daily/0413/instagram/boards/views.py
+++ daily/0413/instagram/boards/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+
 from .models import Board
+from .forms import BoardForm
 
 # Create your views here.
 def index(request):
@@ -8,3 +11,20 @@ def index(request):
         'boards':boards
     }
     return render(request, 'boards/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('boards:index')
+        messages.warning(request, '폼을 다시 확인 후 제출해주세요.')
+    else:
+        form = BoardForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'boards/forms.html', context)
+
+def detail(request, pk):
+    board = get_object_or_404(Board, pk)
