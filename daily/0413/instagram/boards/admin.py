1247dd40dc0b6227d60e18f348876ac9b7368c12
JHyuk2 <whdgurdl77@gmail.com>
2020-04-13 10:03:20 +0900

    Create Board model&admin

--- daily/0413/instagram/boards/admin.py
+++ daily/0413/instagram/boards/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Board
 # Register your models here.
+
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+    list_display_links =['title']
+    list_filter = ['created_at']
+    
+admin.site.register(Board, BoardAdmin)
